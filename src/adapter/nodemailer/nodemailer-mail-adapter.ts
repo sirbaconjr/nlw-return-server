bbd8a1c0c9ea7862c05bd67d50a762efb5ac4f0a
Everton Barbosa Jr <evertonjrccomp@gmail.com>
2022-05-07 18:55:45 -0300

    Aplicou SOLID na aplicação

---
+++ src/adapter/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a1bcf4d547fa97",
+    pass: "74115b4f58d8d3"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Everton Barbosa Jr <evertonjrccomp@gmail.com>',
+      subject: subject,
+      html: body
+    })
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,11 @@
 import express from 'express';
-import { prisma } from './prisma';
-import nodemailer from 'nodemailer';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-var transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "a1bcf4d547fa97",
-    pass: "74115b4f58d8d3"
-  }
-});
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  });
-
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Everton Barbosa Jr <evertonjrccomp@gmail.com>',
-    subject: 'Novo Feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
-
-  return res.status(201).json({data: feedback});
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('HTTP server running');
