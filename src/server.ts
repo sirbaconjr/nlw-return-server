b2095136c6cb0ef341735bded9c4098d8f1b96bf
Everton Barbosa Jr <evertonjrccomp@gmail.com>
2022-05-08 11:46:52 -0300

    Fim da Aula 3
    Adiciona testes
    Adiciona CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
+import { env } from 'process';
 
 const app = express();
 
+app.use(cors({
+  origin: env.FRONTEND_URL
+}));
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333, () => {
